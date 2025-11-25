fb1194863c59fe223691ade572471352a4755987
peter.k <workn.kh14016@gmail.com>
2025-11-25 23:27:46 +0900

    プロジェクト作成

---
+++ excelforbatch/src/main/java/com/example/excelforbatch/ExcelforbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelforbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelforbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelforbatchApplication.class, args);
+	}
+
+}
---
+++ excelforbatch/src/test/java/com/example/excelforbatch/ExcelforbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excelforbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelforbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
